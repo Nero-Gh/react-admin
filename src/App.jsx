2811cf1ee298bf75cd8395effdf1208a71bd2427
Bismark Kadogbe <neroaboagye@gmail.com>
2023-03-07 19:50:48 +0000

    Added header,contact,profile and invoice

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import TopBar from "./scenes/global/TopBar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard/index";
-// import Team from "./scenes/Team";
+import Team from "./scenes/team";
+import Contacts from "./scenes/Contacts";
 // import Invoice from "./scenes/Invoice";
-// import Contacts from "./scenes/Contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/Form";
+
 // import Bar from "./scenes/Bar";
 // import Line from "./scenes/Line";
 // import Pie from "./scenes/Pie";
 // import FAQ from "./scenes/FAQ";
-// import Form from "./scenes/Form";
 // import Geography from "./scenes/FAQ";
 // import Calender from "./scenes/Calender";
 
@@ -29,11 +31,12 @@ function App() {
 
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoice" element={<Invoice />} />
-              <Route path="/faq" element={<FAQ />} />
+              <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              {/*
+              <Route path="/faq" element={<FAQ />} />
               <Route path="/line" element={<Line />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
