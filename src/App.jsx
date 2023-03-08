e627508f64c2ada3adff0499a644b639cb8f6bf9
Bismark Kadogbe <neroaboagye@gmail.com>
2023-03-08 18:54:59 +0000

    Added FAQ page

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ import Contacts from "./scenes/Contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/Form";
 import Calendar from "./scenes/Calendar";
+import FAQ from "./scenes/faq";
 
 // import Bar from "./scenes/Bar";
 // import Line from "./scenes/Line";
 // import Pie from "./scenes/Pie";
-// import FAQ from "./scenes/FAQ";
 // import Geography from "./scenes/FAQ";
 
 function App() {
@@ -36,8 +36,8 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
-              {/*
               <Route path="/faq" element={<FAQ />} />
+              {/*
               <Route path="/line" element={<Line />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
